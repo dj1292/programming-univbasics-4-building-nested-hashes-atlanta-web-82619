a85c4bcbbbcfad90469241028487c572468ef91d
Dan Reeves <djreeves1292@gmail.com>
2019-08-14 19:16:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -43,7 +43,7 @@ def monopoly_with_fourth_tier
        :four_pieces_owned=> 200
      },
      :names=> {
-       :reading_railroad=> {"mortgage_value"},
+       :reading_railroad=> {"mortgage_value"=>"$100"},
        :pennsylvania_railroad=> {"mortgage_value"},
        :b_and_o_railroad=> {"mortgage_value"},
        :shortline_railroad=> {"mortgage_value"}
